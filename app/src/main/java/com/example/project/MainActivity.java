521bb332e798f3c95a2f66e986af046aa8bcb889
tilda <b21tilbe@student.his.se>
2022-05-08 17:48:58 +0200

    Created a textView, button and a shared preferences.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.project;
 
+import android.widget.TextView;
 import android.os.Bundle;
+import android.content.SharedPreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewThing;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textViewThing = findViewById(R.id.name);
+
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("name", "Tilda");
+        editor.apply();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        String name = preferences.getString("name", "inget namn hittades");
+        textViewThing .setText(name);
     }
 
 }
