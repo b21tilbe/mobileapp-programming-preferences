09f9795e98c23cf08e7c408ed0c0d50d83dc2b66
b21tilbe <b21tilbe@student.his.se>
2022-05-13 16:13:32 +0200

    testat mig fram med att koppla second och main activity med varandra

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.content.SharedPreferences;
@@ -25,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString("name", "Tilda");
         editor.apply();
+
+        Button button = findViewById(R.id.start_second_activity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, second_activity.class);
+                intent.putExtra("name", "Tilda");
+                startActivity(intent);
+
+            }
+        });
 
     }
 
